af3486d93726614967277689459c4231acc033db
3239987006@qq.com <3239987006@qq.com>
2018-03-14 21:06:40 +0800

    lesson2-add

--- lesson2/demo01.js
+++ lesson2/demo01.js
@@ -1,3 +1,7 @@
+/*
+**created on 2018/3/11
+*/
+//typeof 一般用于值类型检测
 console.log(typeof 123);//number
 console.log(typeof true);//boolean
 console.log(typeof "abc");//string
@@ -5,15 +9,27 @@ console.log(typeof null);//object
 console.log(typeof undefined);//undefined
 console.log(typeof {name:"Mike",age:20});//object
 
+console.log(typeof function f(){});//function
+console.log(typeof Array);//function
+console.log(typeof Function);//function
+console.log(typeof Date);//function
+console.log(typeof Number);//function
+console.log(typeof String);//function
+console.log(typeof Boolean);//function
 
+console.log(typeof Math);//object
+console.log(typeof JSON);//object
 
+////instanceof（左侧操作数为对象，右侧操作数为原型链中的一个类型时，返回为true）
 var a = {name:"Mike",age:20};
 console.log(a instanceof Object);//true
 
 var b = [12,{},"ab"];
 console.log(b instanceof Array);//true
+console.log(b instanceof Object);//true
 
 var arr= function(){
 };
 var arr1= new arr();
 console.log(arr1 instanceof arr);//true
+console.log(arr1 instanceof Object);//true
